c2f9a9ec22b91f30daf4f9a1dc44ce1aeb40ec08
Luis <luis.roca@ge.com>
2020-08-09 14:38:34 -0400

    Adding vertical order traversal

---
+++ 987-vertical_order_traversal_of_binary_tree/VerticalOrderTraversal.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+
+/**
+ * Definition for a binary tree node.
+ */
+ public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class VerticalOrderTraversal {
+    Dictionary<int, Node> dict;
+    public IList<IList<int>> VerticalTraversal(TreeNode root) {
+        dict = new Dictionary<int, Node>();
+        InOrder(root, 0);
+        
+        return null;
+    }
+
+    public void InOrder(TreeNode root, int x) {
+        if (root == null) return;
+
+        InOrder(root.left, x - 1);
+        
+        if (dict.ContainsKey(x)) {
+            dict[x].numbers.Add(root.val);
+        } else {
+            dict[x] = new Node(x, root.val);
+        }
+        
+        InOrder(root.right, x + 1);
+    }
+
+    private class Node
+    {
+        public int x { get; set; }
+        public IList<int> numbers { get; set; }
+
+        public Node(int x = int.MaxValue, int val = int.MaxValue) {
+            this.x = x;
+            numbers = new List<int> { val };
+        }
+    }
+}
+
+/**
+Given a binary tree, return the vertical order traversal of its nodes values.
+
+For each node at position (X, Y), its left and right children respectively will be at positions (X-1, Y-1) and (X+1, Y-1).
+
+Running a vertical line from X = -infinity to X = +infinity, whenever the vertical line touches some nodes, we report the values of the nodes in order from top to bottom (decreasing Y coordinates).
+
+If two nodes have the same position, then the value of the node that is reported first is the value that is smaller.
+
+Return an list of non-empty reports in order of X coordinate.  Every report will have a list of values of nodes.
+
+ 
+
+Example 1:
+
+
+
+Input: [3,9,20,null,null,15,7]
+Output: [[9],[3,15],[20],[7]]
+Explanation: 
+Without loss of generality, we can assume the root node is at position (0, 0):
+Then, the node with value 9 occurs at position (-1, -1);
+The nodes with values 3 and 15 occur at positions (0, 0) and (0, -2);
+The node with value 20 occurs at position (1, -1);
+The node with value 7 occurs at position (2, -2).
+Example 2:
+
+
+
+Input: [1,2,3,4,5,6,7]
+Output: [[4],[2],[1,5,6],[3],[7]]
+Explanation: 
+The node with value 5 and the node with value 6 have the same position according to the given scheme.
+However, in the report "[1,5,6]", the node value of 5 comes first since 5 is smaller than 6.
+ 
+
+Note:
+
+The tree will have between 1 and 1000 nodes.
+Each node's value will be between 0 and 1000.
+*/
--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace leetcode
         static void Main(string[] args)
         {
             FindAllDuplicates f = new FindAllDuplicates();
+
+            
+            VerticalOrderTraversal v = new VerticalOrderTraversal();
+
             var res = f.FindDuplicates(new int[] {4,3,2,7,8,2,3,1});
             Console.WriteLine(res);
         }
