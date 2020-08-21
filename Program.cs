3d64e1d51200df35d85c10524aec0dc528586d1b
Luis <luis.roca@ge.com>
2020-08-20 23:48:33 -0400

    Reorder List

--- Program.cs
+++ Program.cs
@@ -6,22 +6,10 @@ namespace leetcode
     {
         static void Main(string[] args)
         {
-            FindAllDuplicates f = new FindAllDuplicates();
-
-            
-            VerticalOrderTraversal v = new VerticalOrderTraversal();
-            TreeNode root = new TreeNode(0);
-            root.left = new TreeNode(5);
-            root.left.left = new TreeNode(9);
-            root.right = new TreeNode(1);
-            root.right.left = new TreeNode(2);
-            root.right.left.right = new TreeNode(3);
-            root.right.left.right.left = new TreeNode(4);
-            root.right.left.right.right = new TreeNode(8);
-            root.right.left.right.left.left = new TreeNode(6);
-            root.right.left.right.left.left.left = new TreeNode(7);
-            var res = v.VerticalTraversal(root);
-            Console.WriteLine(res);
+            ReorderLinkedList r = new ReorderLinkedList();
+            var ll = r.CreateLinkedList(new int[]{1,2,3,4});
+            r.ReorderList(ll); 
+            Console.WriteLine(ll);
         }
     }
 }
