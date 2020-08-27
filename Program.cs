2234b6d26714812a4f65a898bd769cbd0e23861f
Luis Roca <luis.roca@ge.com>
2020-08-26 22:44:12 -0400

    Removing contest203 folder

--- Program.cs
+++ Program.cs
@@ -6,10 +6,7 @@ namespace leetcode
     {
         static void Main(string[] args)
         {
-
-            contest203 c = new contest203();
-            var ll = c.FindLatestStep(new int[]{3,5,1,2,4}, 1);
-            Console.WriteLine(ll);
+            Console.WriteLine("OK");
         }
     }
 }
